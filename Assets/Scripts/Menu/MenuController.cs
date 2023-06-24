6dca6f43862c768f462131aac633a79d8d06df73
thank1ess <vladonyaroshchuk@gmail.com>
2023-06-25 01:05:20 +0300

    level loading menu

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class StartController : MonoBehaviour
 {
+    [SerializeField] RectTransform _mainMenu;
+    [SerializeField] RectTransform _loadMenu;
     //public int Level;
     
     public void Play()
@@ -13,8 +16,25 @@ public class StartController : MonoBehaviour
         SceneManager.LoadScene("Level1");
     }
 
+    public void Load()
+    {
+        _mainMenu.gameObject.SetActive(false);
+        _loadMenu.gameObject.SetActive(true);
+    }
+
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(EventSystem.current.currentSelectedGameObject.name);
+    }
+
+    public void BackToMenu()
+    {
+        _mainMenu.gameObject.SetActive(true);
+        _loadMenu.gameObject.SetActive(false);
     }
 }
